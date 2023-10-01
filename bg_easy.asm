538730ef0d474b19bec8eb52f9b3873809cbf4ce
andrewdempsey2018 <andrewdempsey2016@gmail.com>
2023-10-01 21:31:21 +0100

    update comments in bg_easy.asm

--- bg_easy.asm
+++ bg_easy.asm
@@ -21,7 +21,7 @@ reset:
     ldx #$40
     stx $4017
 
-;initialize stack register
+;initialise stack register
     ldx #$ff
     txs
 
@@ -63,12 +63,15 @@ clearMem:
     bpl :-
 
 ;prep ppu $3f10 - $3f1f for sprite and background palettes
+;$2006 is the ppu address register (aka 'ppuADDR').
+;we first set the high byte of the address in the ppu we want to write to and then set the low byte
+;we then start writing to the ppu with $2007 ($2007 is the ppuDATA read buffer)
     lda #$3f
     sta $2006
     lda #$00
     sta $2006
 
-   ldx #$00
+    ldx #$00
 
 ;load in the palettes
 loadPalettes:
@@ -78,35 +81,43 @@ loadPalettes:
     cpx #$20
     bne loadPalettes
 
+;using the ppuADDR and the ppuDATA read buffer ($2006 and $2007)
+;tell the ppu we are going to start filling up the first nametable (which starts at $2000 in ppu ram)
 loadBackground:
-    lda $2002             ; read ppu status to reset the high/low latch
+    lda $2002 ;read ppu status to reset the high/low latch
     lda #$20
-    sta $2006             ; write the high byte of $2000 address
+    sta $2006 ;write the high byte of $2000 address
     lda #$00
-    sta $2006             ; write the low byte of $2000 address
-    ldx #$00              ; start out at 0
+    sta $2006 ;write the low byte of $2000 address
+    ldx #$00
 
 loadBackgroundLoop:
-      lda nametable, x     ; load data from address (background + the value in x)
-      sta $2007             ; write to ppu
-      inx                   ; x = x + 1
-      cpx #$80              ; compare x to hex $80, decimal 128 - copying 128 bytes
-      bne loadBackgroundLoop  ; branch to loadbackgroundloop if compare was not equal to zero
+    lda nametable, x ;load data from address (nametable + the value in x)
+    sta $2007 ;write to ppu
+    inx
+    cpx #$80 ;compare x to hex $80, decimal 128 - copying 128 bytes
+    bne loadBackgroundLoop ;branch to loadBackgroundLoop if compare was not equal to zero
 
+;each nametable has an associated attribute table
+;the nametable takes up 960 bytes and the remaining 64 bytes belong to the attribute table (960 + 64 = 1,024)
+;as we are concerned with the first nametable here, the attributes start at $23c0 in the ppu ($2000 + 960)
+;as the first nametable starts at $2000 and takes up 960 bytes, the attributes start at $23c0 (960 decimal = 3c0 hex)
 loadAttribute:
-      lda $2002             ; read ppu status to reset the high/low latch
-      lda #$23
-      sta $2006             ; write the high byte of $23c0 address
-      lda #$c0
-      sta $2006             ; write the low byte of $23c0 address
-      ldx #$00              ; start out at 0
+    lda $2002 ;read ppu status to reset the high/low latch
+    lda #$23
+    sta $2006 ;write the high byte of $23c0 address
+    lda #$c0
+    sta $2006 ;write the low byte of $23c0 address
+    ldx #$00 ;start out at 0
 
+;in this simple demonstration, we are only loading in 128 tiles. therefore, we only need to load
+;in 8 attribute values.
 loadAttributeLoop:
-      lda attribute, x      ; load data from address (attribute + the value in x)
-      sta $2007             ; write to ppu
-      inx                   ; x = x + 1
-      cpx #$0f              ; compare x to hex $08, decimal 8 - copying 8 bytes
-      bne loadAttributeLoop
+    lda attribute, x ;load data from address (attribute + the value in x)
+    sta $2007 ;write to ppu
+    inx
+    cpx #$08 ;compare x to hex $08, decimal 8 - copying 8 bytes
+    bne loadAttributeLoop
 
 ;enable interrupts
     cli
@@ -147,7 +158,6 @@ nametable:
 
 attribute:
     .byte %00011011, %00000000, %00000000, %00000000, %00000000, %00000000, %00000000, %00011011
-    .byte %00000000, %00000000, %00000000, %00000000, %00000000, %00000000, %00000000, %00000000
 
 .segment "ZEROPAGE"
 
